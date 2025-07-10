d755a369a0aae885c8fd4d2ca47697d59c533cad
Anna Madeira <69610009+Anna-Madeira@users.noreply.github.com>
2025-07-10 14:51:06 -0300

    ajustes pagina direito de familia

--- src/app/areas-atuacao/direito-familia/page.tsx
+++ src/app/areas-atuacao/direito-familia/page.tsx
@@ -24,10 +24,44 @@ export default function atuation(){
             </h2>
             <div className="grid grid-cols-1 gap-8">
               <div className="bg-gray-100 p-8 rounded-lg">
-                <p className="text-gray-700 mb-4">
-                  Prestamos assessoria jurídica personalizada para casos de divórcios, pensão alimentícia, guarda de filhos e união estável.
-                </p>
-                </div>
+                <p className="text-gray-700 mb-4  italic">
+                  O Direito de Família lida com as relações mais íntimas, exigindo um olhar humano e estratégico. A Dra. Teresa Cristina Madeira oferece suporte jurídico completo e empático para guiar você em cada etapa, sempre buscando o bem-estar de todos os envolvidos, especialmente das crianças.
+                    Nossa atuação especializada abrange:</p>
+                  <ul className="list-inside mb-4 text-center">
+                        <li className='mb-2 italic'>
+                          <span className="font-bold">Divórcio e Separação:</span> Orientação para dissolução do casamento, seja consensual ou litigiosa.
+                        </li>
+                        <li className='mb-2 italic'>
+                          <span className="font-bold">Guarda e Convivência:</span> Definição da guarda dos filhos e do regime de visitação, priorizando o desenvolvimento das crianças.
+                        </li>
+                        <li className='mb-2 italic'>
+                          <span className="font-bold">Pensão Alimentícia:</span> Fixação, revisão ou exoneração, assegurando o sustento e a justa contribuição.
+                        </li>
+                        <li className='mb-2 italic'>
+                          <span className="font-bold">União Estável:</span> Reconhecimento, dissolução e conversão, com foco nos aspectos patrimoniais.
+                        </li>
+                        <li className='mb-2 italic'>
+                          <span className="font-bold">Paternidade e Filiação:</span> Ações de investigação e reconhecimento, garantindo os direitos de filiação.
+                        </li>
+                        <li className='mb-2 italic'>
+                          <span className="font-bold">Partilha de Bens:</span> Divisão do patrimônio em casos de divórcio, união estável ou sucessão.
+                        </li>
+                        <li className='mb-2 italic'>
+                          <span className="font-semibold">Inventário e Sucessões:</span> Apoio em processos de herança e testamentos.
+                        </li>
+                      </ul>
+                    <p className='mb-4  italic'>
+                      Nosso time se dedica a oferecer orientação clara e soluções eficazes, proporcionando segurança e tranquilidade para que você possa seguir em frente com confiança.
+                    </p>
+                    <div className="flex justify-center items-center">
+                     <Link
+                        href='/Contato'
+                          className="bg-yellow-400 hover:bg-yellow-500 text-blue-900 font-bold py-3 items-center justify-items-center
+                          px-8 rounded-full text-lg md:text-xl transition duration-300 ease-in-out transform hover:scale-105 shadow-lg">
+                          Entre em contato conosco!
+                       </Link>
+                       </div>
+                </div> 
                 </div>
                 </div>
                 </main>
